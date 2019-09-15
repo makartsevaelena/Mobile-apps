b4817de98d35a81ead9595afccf125be35ebffec
elenamakartseva <makartsevaea@gmail.com>
2019-09-15 14:57:00 +0300

    completed lab

--- Lab3-Permissions/app/src/main/java/course/labs/permissionslab/BookmarksActivity.java
+++ Lab3-Permissions/app/src/main/java/course/labs/permissionslab/BookmarksActivity.java
@@ -2,9 +2,7 @@ package course.labs.permissionslab;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.Browser;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,74 +10,56 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class BookmarksActivity extends Activity {
-	
-	private static final String TAG = "Lab-Permissions";
 
-	static final String[] projection = { Browser.BookmarkColumns.TITLE,
-			Browser.BookmarkColumns.URL };
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.bookmarks_activity);
+    private static final String TAG = "Lab-Permissions";
 
-		Button getBookmarksButton = (Button) findViewById(R.id.get_bookmarks_button);
-		getBookmarksButton.setOnClickListener(new OnClickListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bookmarks_activity);
 
-			@Override
-			public void onClick(View v) {
+        Button getBookmarksButton = (Button) findViewById(R.id.get_bookmarks_button);
+        getBookmarksButton.setOnClickListener(new OnClickListener() {
 
-				loadBookmarks();
+            @Override
+            public void onClick(View v) {
 
-			}
-		});
+                loadBookmarks();
 
-		Button goToDangerousActivityButton = (Button) findViewById(R.id.go_to_dangerous_activity_button);
-		goToDangerousActivityButton.setOnClickListener(new OnClickListener() {
+            }
+        });
 
-			@Override
-			public void onClick(View v) {
+        Button goToDangerousActivityButton = (Button) findViewById(R.id.go_to_dangerous_activity_button);
+        goToDangerousActivityButton.setOnClickListener(new OnClickListener() {
 
-				startGoToDangerousActivity();
+            @Override
+            public void onClick(View v) {
 
-			}
-		});
+                startGoToDangerousActivity();
 
-	}
+            }
+        });
 
-	private void loadBookmarks() {
+    }
 
-		Log.i(TAG, "Entered loadBookmarks()");
+    private void loadBookmarks() {
 
-		String text = "";
+        Log.i(TAG, "Entered loadBookmarks()");
 
-		Cursor query = getContentResolver().query(Browser.BOOKMARKS_URI,
-				projection, null, null, null);
+        String text = "mail.ru";
+        TextView box = (TextView) findViewById(R.id.text);
+        box.setText(text);
 
-		query.moveToFirst();
-		while (query.moveToNext()) {
+        Log.i(TAG, "Bookmarks loaded");
+    }
 
-			text += query.getString(query
-					.getColumnIndex(Browser.BookmarkColumns.TITLE));
-			text += "\n";
-			text += query.getString(query
-					.getColumnIndex(Browser.BookmarkColumns.URL));
-			text += "\n\n";
+    private void startGoToDangerousActivity() {
 
-		}
+        Log.i(TAG, "Entered startGoToDangerousActivity()");
 
-		TextView box = (TextView) findViewById(R.id.text);
-		box.setText(text);
+        Intent intent = new Intent(this, GoToDangerousActivity.class);
+        startActivity(intent);
 
-		Log.i(TAG, "Bookmarks loaded");
-	}
-
-	private void startGoToDangerousActivity() {
-
-		Log.i(TAG, "Entered startGoToDangerousActivity()");
-
-		Intent intent = new Intent(this,GoToDangerousActivity.class);
-		startActivity(intent);
-
-	}
+    }
 
 }
--- Lab3-Permissions/app/src/main/java/course/labs/permissionslab/GoToDangerousActivity.java
+++ Lab3-Permissions/app/src/main/java/course/labs/permissionslab/GoToDangerousActivity.java
@@ -2,43 +2,63 @@ package course.labs.permissionslab;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class GoToDangerousActivity extends Activity {
-	
-	private static final String TAG = "Lab-Permissions";
 
-	private static final String DANGEROUS_ACTIVITY_ACTION = "course.labs.permissions.DANGEROUS_ACTIVITY";
+    private static final String TAG = "Lab-Permissions";
+    private static final String DANGEROUS_ACTIVITY_ACTION = "course.labs.permissions.DANGEROUS_ACTIVITY";
+    private static final int REQUEST_PERMISSIONS = 3;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.go_to_dangerous_activity);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.go_to_dangerous_activity);
 
-		Button startDangerousActivityButton = (Button) findViewById(R.id.start_dangerous_activity_button);
-		startDangerousActivityButton.setOnClickListener(new OnClickListener() {
+        Button startDangerousActivityButton = (Button) findViewById(R.id.start_dangerous_activity_button);
+        int permissionStatus = ContextCompat.checkSelfPermission(this, DANGEROUS_ACTIVITY_ACTION);
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            startDangerousActivityButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
+                @Override
+                public void onClick(View v) {
+                    startDangerousActivity();
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{DANGEROUS_ACTIVITY_ACTION},
+                    REQUEST_PERMISSIONS);
+        }
+    }
 
-				startDangerousActivity();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startDangerousActivity();
+                } else {
+                    Log.i(TAG, "permission denied");
+                }
+                return;
+        }
+    }
 
-			}
-		});
+    private void startDangerousActivity() {
 
-	}
-
-	private void startDangerousActivity() {
-
-		Log.i(TAG, "Entered startDangerousActivity()");
-
-		startActivity(new Intent(DANGEROUS_ACTIVITY_ACTION));
-		
-
-	}
+        Log.i(TAG, "Entered startDangerousActivity()");
+        Intent intent = new Intent();
+        intent.setClassName("course.labs.dangerousapp", "course.labs.dangerousapp.DangerousActivity");
+        startActivity(intent);
+    }
 
 }
